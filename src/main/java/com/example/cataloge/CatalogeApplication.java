88656739bfbfa91d0665ba5646a2800131cb1098
Lehkdup <s.lehkdup@gmail.com>
2024-10-25 11:15:57 +0200

    Initial Commit SpringBoot integration

---
+++ src/main/java/com/example/cataloge/CatalogeApplication.java
@@ -0,0 +1,13 @@
+package com.example.cataloge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatalogeApplication.class, args);
+    }
+
+}
