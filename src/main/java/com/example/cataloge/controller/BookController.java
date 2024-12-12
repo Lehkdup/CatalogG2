0da8d4dba2c8838dc8d484b10299f47786f5b59b
Jakob Koller <jaki.koller@gmail.com>
2024-12-12 15:21:27 +0100

    Fixing fallback methods

--- src/main/java/com/example/cataloge/controller/BookController.java
+++ src/main/java/com/example/cataloge/controller/BookController.java
@@ -21,12 +21,12 @@ public class BookController {
 
     @PostMapping(consumes = "application/json", produces = "application/json")
     @Retry(name = "retryApi", fallbackMethod = "fallbackAfterRetry")
-    public ResponseEntity createBook(@RequestBody Book book){
+    public ResponseEntity createBook(@RequestBody Book book) {
         System.out.println(book.getISBN());
-        if(book.getISBN() == null){
+        if (book.getISBN() == null) {
             return ResponseEntity.badRequest().body("insufficient content provided");
         } else {
-            if(book.getISBN().equals(bookRepository.findBookByISBN(book.getISBN()))){
+            if (book.getISBN().equals(bookRepository.findBookByISBN(book.getISBN()))) {
                 return ResponseEntity.status(HttpStatus.CONFLICT).body("Book with this ISBN already exists");
             } else {
                 book = bookRepository.save(book);
@@ -34,49 +34,55 @@ public class BookController {
             }
         }
     }
-    private String fallbackCreateBook(Book book, Throwable throwable){
-        return "an error occured while creating the book " + book.getTitle();
+
+    private ResponseEntity fallbackCreateBook(Book book, Throwable throwable) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body("an error occured while creating the book " + book.getTitle());
     }
 
     @GetMapping(path = "/search/{searchTerm}")
     @Retry(name = "retryApi", fallbackMethod = "fallbackSearchBook")
-    public List<Book> searchBook(@PathVariable String searchTerm){
+    public List<Book> searchBook(@PathVariable String searchTerm) {
         List<Book> searchResult = bookRepository.searchBooks(searchTerm);
         return searchResult;
     }
-    private String fallbackSearchBook(String searchTerm, Throwable throwable){
-        return "an error occured while searching for: " + searchTerm;
+
+    private List<Book> fallbackSearchBook(String searchTerm, Throwable throwable) {
+        return new ArrayList<>();
     }
 
     @GetMapping
-    @Retry(name = "retryApi", fallbackMethod = "fallbackAfterRetry")
-    public List<Book> getAllBooks(){
+    @Retry(name = "retryApi", fallbackMethod = "fallbackGetAllBooks")
+    public List<Book> getAllBooks() {
         List<Book> allBooks = bookRepository.findAll();
         return allBooks;
     }
 
+    private List<Book> fallbackGetAllBooks(String searchTerm, Throwable throwable) {
+        return new ArrayList<>();
+    }
+
     @GetMapping(path = "{id}")
     @Retry(name = "retryApi", fallbackMethod = "fallbackBookById")
-    public Optional<Book> getBookById(@PathVariable Long id){
+    public Optional<Book> getBookById(@PathVariable Long id) {
         Optional<Book> book = bookRepository.findById(id);
         return book;
     }
-    private String fallbackBookById(Long id, Throwable throwable){
-        return "an error occured while getting your book with id: " + id;
+
+    private Optional<Book> fallbackBookById(Long id, Throwable throwable) {
+        return Optional.empty();
     }
 
     @PostMapping(path = "/inCart")
     @Retry(name = "retryApi", fallbackMethod = "fallbackBooksInCart")
-    public ResponseEntity getBooksInCart(@RequestBody GetBooksInCartWrapper requestWrapper){
+    public ResponseEntity getBooksInCart(@RequestBody GetBooksInCartWrapper requestWrapper) {
         List<Long> bookIdsInCart = requestWrapper.getBookIds();
         List<Book> booksInCart = bookRepository.findAllById(bookIdsInCart);
         return ResponseEntity.ok(booksInCart);
     }
-    private String fallbackBooksInCart(GetBooksInCartWrapper requestWrapper, Throwable throwable){
-        return "an error occured while retrieving your cart " + requestWrapper.getBookIds().toString();
-    }
 
-    private String fallbackAfterRetry(Exception exception){
-        return "an error occured, please try again later";
+    private ResponseEntity fallbackBooksInCart(GetBooksInCartWrapper requestWrapper, Throwable throwable) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body("an error occured while retrieving your cart " + requestWrapper.getBookIds().toString());
     }
 }
